0066f6e9003288602fe75e11ce215befce3a62bd
Caleb Harnell <calebharnell@hotmail.com>
2021-02-09 17:21:12 +1000

    Update <br>

--- src/renderSchema.js
+++ src/renderSchema.js
@@ -29,7 +29,7 @@ function renderObject(type, options) {
   const isInputObject = type.kind === 'INPUT_OBJECT'
 
   if (!skipTitle) {
-    printer(`\n\n<br>\n`)
+    printer(`\n<br>`)
     printer(`\n${'#'.repeat(headingLevel + 2)} ${type.name}\n`)
   }
   if (type.description) {
@@ -192,6 +192,7 @@ function renderSchema(schema, options) {
         printer(`    * [${type.name}](#${type.name.toLowerCase()})`)
       })
     }
+    printer(`<br>`)
   }
 
   if (query) {
